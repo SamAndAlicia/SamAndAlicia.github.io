08ef904c28c306bef4a7b1e06d47f06d0452d74b
Ethan Archer <ethanarcher1@gmail.com>
2022-02-15 22:02:16 +0000

    adding the rsvp page and the countdown timer

---
+++ assets/js/dietary_requirements.js
@@ -0,0 +1,7 @@
+function CheckDietaryRequirements(val){
+ var element=document.getElementById('dietary_requirements_details');
+ if(val=='yes')
+   element.style.display='block';
+ else
+   element.style.display='none';
+}
